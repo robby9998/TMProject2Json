3b7f97321269f0052c9238dc9d55d7a5fd6ce29d
Robby Shower <robby.shower@gmx.de>
2022-03-24 01:53:33 +0100

    FINAL 1.0.0.3

--- Project2Json/My Project/Application.Designer.vb
+++ Project2Json/My Project/Application.Designer.vb
@@ -23,9 +23,9 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Public Sub New()
-            MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.ApplicationDefined)
+            MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
             Me.IsSingleInstance = true
-            Me.EnableVisualStyles = true
+            Me.EnableVisualStyles = false
             Me.SaveMySettingsOnExit = true
             Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
         End Sub
